4d358b9c17b2c5a8ade892541d56e7720c92d7f4
Kaung Zaw Htet <kaungzawhtet.mm@gmail.com>
2025-05-21 04:40:21 +0700

    feat: userPoolClientId updated

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-southeast-1_Mtt2FaTw9', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'v52c6sq9ujm5ggl08r29ui648', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6lovim10eg6413r5ld7paaln48', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
