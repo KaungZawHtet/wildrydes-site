41d324889ebbd181c514258f053e33ffca1f13ec
Kaung Zaw Htet <kaungzawhtet.mm@gmail.com>
2025-05-21 04:13:06 +0700

    feat: cognito config included

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_Mtt2FaTw9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'v52c6sq9ujm5ggl08r29ui648', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
